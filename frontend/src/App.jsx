84e8a4e63117d180e2676298878e6f8759b2c0d4
chloeafp <chloeafp@gmail.com>
2023-06-28 16:18:39 +0200

    initialisation projet

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,10 @@
+import NavBar from "./components/NavBar"
 function App() {
 
 
   return (
     <>
+      <NavBar />
       <p className='text-2xl'>
         Click on the Vite and React logos to learn more
       </p>

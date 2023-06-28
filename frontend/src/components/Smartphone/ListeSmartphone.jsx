7112d57ba91cb91c694b8ad2bd237b6999229ae9
chloeafp <chloeafp@gmail.com>
2023-06-29 01:20:09 +0200

    details smartphone + smartphone context

---
+++ frontend/src/components/Smartphone/ListeSmartphone.jsx
@@ -0,0 +1,67 @@
+import React, { useContext } from "react";
+import { AiFillEye } from "react-icons/ai";
+import Menu_filtre from "../menu_filtrage/MenuFiltre";
+import SmartphoneContext from "../contexts/SmartphoneContext";
+import { Link } from "react-router-dom";
+
+const ListeSmartphone = () => {
+  const { smartphoneData } = useContext(SmartphoneContext);
+
+  return (
+    <div className="flex justify-between">
+      <Menu_filtre className="w-1/3" />
+      <div className="pt-10 flex flex-col gap-10  w-2/3 mx-3">
+        <div className="flex justify-between items-center">
+          <p className="text-black font-bold">Base de données</p>
+          <div className="flex justify-around gap-10">
+            <button className="bg-[#e52460] hover:bg-[#e698b1] text-white font-bold py-2 px-4 rounded-3xl">
+              Exporter un fichier
+            </button>
+            <button className="bg-[#00b3b6] hover:bg-[#068284] text-white font-bold py-2 px-4 rounded-3xl">
+              Importer un fichier
+            </button>
+          </div>
+        </div>
+        <table className="table-auto">
+          <thead className="text-left bg-[#f9c838]">
+            <tr>
+              <th>Marque</th>
+              <th>Modèle</th>
+              <th>Catégorie</th>
+              <th>Date d'ajout</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            {smartphoneData &&
+              smartphoneData.map((phone) => {
+                return (
+                  <tr className="even:bg-gray-50 odd:bg-white font-light">
+                    <td>{phone.marque}</td>
+                    <td>{phone.modele}</td>
+                    <td>{phone.categorie_prix}</td>
+                    <td>
+                      {phone.date_ajout
+                        .split("")
+                        .slice(0, 10)
+                        .join("")
+                        .split("-")
+                        .reverse()
+                        .join("/")}
+                    </td>
+                    <Link to={`/smartphone_details/${phone.id}`}>
+                      <td>
+                        <AiFillEye />
+                      </td>
+                    </Link>
+                  </tr>
+                );
+              })}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+};
+
+export default ListeSmartphone;
--- frontend/src/navigation/Router.jsx
+++ frontend/src/navigation/Router.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
-import ListeSmartphone from '../components/liste_smartphone/ListeSmartphone';
+import ListeSmartphone from '../components/Smartphone/ListeSmartphone';
 import { Routes, Route} from "react-router-dom";
+import SmartphoneDetails from '../components/Smartphone/SmartphoneDetails';
 
 const Router = () => {
     return (
         <Routes>
              <Route path="/smartphone" element={<ListeSmartphone />} />
+             <Route path="/smartphone_details/:id" element={<SmartphoneDetails />} />
+
         </Routes>
     );
 };

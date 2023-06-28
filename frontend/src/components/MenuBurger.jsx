f5dacec5b30c6910c73966d663984705996f0ba8
chloeafp <chloeafp@gmail.com>
2023-06-28 17:11:40 +0200

    navbar + burger

--- frontend/src/components/MenuBurger.jsx
+++ frontend/src/components/MenuBurger.jsx
@@ -27,16 +27,16 @@ function BurgerMenu() {
           <div className="absolute w-full h-44 py-4 right-0 px-3 bg-[#F9F9F9] shadow-b-[0px_1px_5px_2px_#00000024] top-20 rounded-md transition m-auto">
             <ul>
               <li className="p-2 hover:text-[#E52460] font-medium">
-                <NavLink to="/decouvrir">Mon profil</NavLink>
+                <NavLink to="">Mon profil</NavLink>
               </li>
               <li className="p-2 hover:text-[#E52460] font-medium">
                 <NavLink>Ajouter un téléphone</NavLink>
               </li>
               <li className="p-2 hover:text-[#E52460] font-medium">
-                <NavLink to="/admin">Base de données</NavLink>
+                <NavLink to="">Base de données</NavLink>
               </li>
               <li className="p-2 hover:text-[#E52460] font-medium">
-                <NavLink to="/admin">FAQ</NavLink>
+                <NavLink to="">FAQ</NavLink>
               </li>
             </ul>
           </div>

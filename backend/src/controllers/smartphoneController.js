cdfb9f49d79cd426f79a2d020f9458fd9c4fdc98
Boivinos <boivinantonin@gmail.com>
2023-06-29 18:38:48 +0200

    delete route front + back

--- backend/src/controllers/smartphoneController.js
+++ backend/src/controllers/smartphoneController.js
@@ -38,6 +38,23 @@ const findOne = (req, res) => {
     });
 };
 
+const destroy = (req, res) => {
+  const id = parseInt(req.params.id);
+  database
+    .query("delete from smartphone where id =?", [id])
+    .then(([result]) => {
+      if (result.affectedRows !== 0) {
+        res.sendStatus(204);
+      } else {
+        res.status(404).send("Not Found");
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error retrieving data from database");
+    });
+};
+
 const postPhone = (req, res) => {
   database
     .query(
@@ -97,4 +114,5 @@ module.exports = {
   findAll,
   findOne,
   postPhone,
+  destroy,
 };

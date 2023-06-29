c752fd1222fd262c9640891c302fb2dba9d352b6
Headonnn <[th.couraud@gmail.com](th.couraud@gmail.com)>
2023-06-29 12:34:19 +0200

    style du form et algo etat tel

--- backend/src/router.js
+++ backend/src/router.js
@@ -1,10 +1,27 @@
 const express = require("express");
-
+const fs = require('fs')
 const router = express.Router();
+
+const multer = require("multer");
+const upload = multer({ dest: "./public/uploads/" });
 
 const smartphoneControllers = require("./controllers/smartphoneController");
 
 router.get("/api/smartphone", smartphoneControllers.findAll);
 router.get("/api/smartphone/:id", smartphoneControllers.findOne);
+
+
+
+router.post("/api/avatar", upload.single("avatar"), (req, res) => {
+
+    const { originalname } = req.file;
+    console.log(req.file.originalname)
+    const { filename } = req.file;
+    fs
+        .rename(`./public/uploads/${filename}`, `./public/uploads/${originalname}`, (err) => {
+            if (err) throw err;
+            res.send("File uploaded");
+        });
+})
 
 module.exports = router;
--- frontend/src/components/NavBar/NavBar.jsx
+++ frontend/src/components/NavBar/NavBar.jsx
@@ -1,19 +1,19 @@
 import React from 'react';
 import { Link, NavLink } from 'react-router-dom';
 import logo from '../../assets/logo-200.png'
-import {BsPersonCircle} from 'react-icons/bs';
+import { BsPersonCircle } from 'react-icons/bs';
 import BurgerMenu from './MenuBurger';
 
 const NavBar = () => {
-    return (
-        <nav className='flex justify-between items-center h-24 bg-[#F9F9F9] px-6 shadow-[0px_1px_5px_2px_#00000024]'>
-             <Link to="/">
-            <img className="w-48" src={logo} alt="Logo Emmaus Connect" />
-          </Link>
-          <ul className="flex items-center gap-24">
-    
+  return (
+    <nav className='flex justify-between items-center h-24 bg-[#F9F9F9] px-6 shadow-[0px_1px_5px_2px_#00000024]'>
+      <Link to="/">
+        <img className="w-48" src={logo} alt="Logo Emmaus Connect" />
+      </Link>
+      <ul className="flex items-center gap-24">
+
         <li className="text-l hidden lg:block tracking-wide hover:text-[#E52460] transition">
-          <NavLink to="">Ajouter un téléphone</NavLink>
+          <NavLink to="/ajout">Ajouter un téléphone</NavLink>
         </li>
         <li className="text-l hidden lg:block tracking-wide hover:text-[#E52460] transition">
           <NavLink to="/smartphone">Base de données</NavLink>
@@ -27,8 +27,8 @@ const NavBar = () => {
         <p className='hidden lg:block'>Profile</p>
         <BurgerMenu className="block md:hidden" />
       </div>
-        </nav>
-    );
+    </nav>
+  );
 };
 
 export default NavBar;

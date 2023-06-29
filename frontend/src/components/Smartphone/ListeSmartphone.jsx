d1776e6857d121643199fa55561962b935a0a187
chloeafp <chloeafp@gmail.com>
2023-06-29 19:43:23 +0200

    icon poubelle

--- frontend/src/components/Smartphone/ListeSmartphone.jsx
+++ frontend/src/components/Smartphone/ListeSmartphone.jsx
@@ -18,11 +18,11 @@ const ListeSmartphone = () => {
     console.log(id);
     axios
       .delete(`${import.meta.env.VITE_BACKEND_URL}/api/smartphone/${id}`)
-      .then(() => setSmartPhoneData(smartphoneData.filter((el)=> el.id !== id)))
+      .then(() =>
+        setSmartPhoneData(smartphoneData.filter((el) => el.id !== id))
+      )
       .catch((error) => console.error(error.message));
-  }
-
-
+  };
 
   return (
     <>
@@ -72,7 +72,7 @@ const ListeSmartphone = () => {
                         }
                       }
                       return false;
-                       }
+                    }
                     return true;
                   })
                   .filter((phone) =>
@@ -108,7 +108,6 @@ const ListeSmartphone = () => {
                           <td className="text-red-700">375€ et plus</td>
                         )}
 
-                        
                         <td>
                           {phone.date_ajout
                             .split("")
@@ -119,22 +118,38 @@ const ListeSmartphone = () => {
                             .join("/")}
                         </td>
 
-
-                    <td>
-                      <Link to={`/smartphone_details/${phone.id}`}>
-                        <AiOutlineInfoCircle className="text-2xl hover:text-[#e52460] transition hover:scale-110"/>
-                      </Link>
-                    </td>
-                    <td>                      
-                        <ImBin className="text-2xl hover:text-[#e52460] transition scale-75 hover:scale-100 cursor-pointer"
-                        onClick={()=> handleDelete(phone.id)}/>                     
-                    </td>
-                  </tr>
-                );
-              })}
-          </tbody>
-        </table>
-      </div>
+                        <td>
+                          <Link to={`/smartphone_details/${phone.id}`}>
+                            <AiOutlineInfoCircle className="text-2xl hover:text-[#e52460] transition hover:scale-110" />
+                          </Link>
+                        </td>
+                        <td className="flex items-center">
+                          <button
+                            type="button"
+                            onClick={() => handleDelete(phone.id)}
+                          >
+                            <svg
+                              xmlns="http://www.w3.org/2000/svg"
+                              fill="none"
+                              viewBox="0 0 24 24"
+                              strokeWidth="1.5"
+                              stroke="currentColor"
+                              className="h-5 hover:text-[#e52460] transition hover:scale-105"
+                            >
+                              <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
+                              />
+                            </svg>
+                          </button>
+                        </td>
+                      </tr>
+                    );
+                  })}
+            </tbody>
+          </table>
+        </div>
       </div>
     </>
   );
